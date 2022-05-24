444c56f88e5205dc6a7ee5ea1f25d6f5b0593359
vxran <vince.garces@gmail.com>
2022-05-24 13:19:46 +0800

    test

--- edit.php
+++ edit.php
@@ -45,7 +45,7 @@
                 <div class="form-group">
                     <label for="name">Name</label>
 
-                    <input type="text" class="form-control" id="name" value="<?php $_GET['edit'];?>" placeholder="Enter name">
+                    <input type="text" class="form-control" id="name" value="wew" placeholder="Enter name">
                     <small class="form-text text-muted">Help message here.</small>
                 </div>
                 <button type="submit" class="btn btn-primary">Submit</button>
