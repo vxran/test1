42d19dbb6e245ab9ce2222eef080ddbc4c7cd048
vxran <vince.garces@gmail.com>
2022-05-24 17:02:46 +0800

    test

--- edit.php
+++ edit.php
@@ -45,7 +45,7 @@
                 <div class="form-group">
                     <label for="name">Name</label>
 
-                    <input type="text" class="form-control" id="name" value="<?php echo $_GET['edit'];?>" placeholder="Enter name">
+                    <input type="text" class="form-control" id="name" value="<?php echo $_GET['first']; echo ' '; echo $_GET['last'];?>" placeholder="Enter name">
                     <small class="form-text text-muted">Help message here.</small>
                 </div>
                 <button type="submit" class="btn btn-primary">Submit</button>
--- index.php
+++ index.php
@@ -88,10 +88,10 @@ function getRow(x) {
 			}
                         // echo '<a href="view.php"><button class="btn btn-primary btn-sm">View</button></a>';
 
-                        echo '<a href="view.php?view='.$row["first"]." ".$row["last"].'&id='.$row["id"
-                    ].'"><button class="btn btn-primary btn-sm">View</button></a>';
+                        echo '<a href="view.php?view='.$row["first"]." ".$row["last"].'"><button class="btn btn-primary btn-sm">View</button></a>';
 
-                        echo '<a href="edit.php?edit='.$row["first"]." ".$row["last"].'"><button class="btn btn-outline-primary btn-sm">Edit</button></a>';
+                        echo '<a href="edit.php?first='.$row["first"].'&last='.$row["last"].'&id='.$row["id"
+                    ].'"><button class="btn btn-outline-primary btn-sm">Edit</button></a>';
 
                        echo '<a href="index.php?delete='.$row["id"].'"><button class="btn btn-outline-primary btn-sm">delete</button></a>';
 
